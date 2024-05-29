4fe10db96bb2dc2dfc150abab956a879f33dc6d8
luisdibdin <luisdibdin@icloud.com>
2024-05-29 21:02:37 +0100

    add test lambda with layer

--- iam.tf
+++ iam.tf
@@ -58,3 +58,22 @@ resource "aws_iam_role_policy" "github_actions_policy" {
     ]
   })
 }
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
