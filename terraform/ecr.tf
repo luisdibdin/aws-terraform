27dae31b69eef86b988db4440498f8c35018b70b
Luis Dibdin <60894202+luisdibdin@users.noreply.github.com>
2024-12-07 21:26:50 +0000

    tf no changes

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "lambda" {
-  name                 = "lambda-test"
+  name                 = "lambda"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
